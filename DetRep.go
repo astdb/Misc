e5ca86c6a166eecdbbb50a5784ab11d6ce1bf8b0
Nirmal <astdb@users.noreply.github.com>
2017-03-02 20:59:02 +1100

    Add O(n) detection function (findReps)

--- DetRep.go
+++ DetRep.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-    numbers := []int{85, 10, 9, 8, 74, 37, 93, 89, 19, 56, 26, 63, 9, 35, 87, 24, 16, 27, 17, 55, 31, 7, 5, 45, 14, 38, 88, 2, 69, 4, 43, 46, 53, 32, 11, 86, 80, 47, 3, 71, 97, 49, 13, 56, 25, 66, 60, 98, 92, 18, 58, 56, 44, 44, 44, 44}
-    err := detectRepeats(numbers)
+    numbers := []int{85, 10, 9, 8, 74, 37, 93, 89, 19, 56, 26, 63, 9, 35, 87, 24, 16, 27, 17, 55, 31, 7, 5, 45, 14, 38, 88, 2, 69, 4, 43, 46, 53, 32, 11, 86, 80, 47, 3, 71, 97, 49, 13, 56, 25, 66, 60, 98, 92, 18, 58, 56, 44, 44, 44, 56}
+    // err := detectRepeats(numbers)
+    err := findReps(numbers)
 
     if err != nil {
         fmt.Printf("Error: %v\n", err)
@@ -14,6 +15,34 @@ func main() {
         fmt.Println("No >3 repeat numbers")
     }
 }
+
+func findReps(numbers []int) error {
+  numberCounts := map[int]int{}
+  
+  for _, num := range numbers {
+      // fmt.Printf("Top range going through %d\n", num)      
+    _, found := numberCounts[num]
+
+   if found {
+       // fmt.Printf("%d found in cache - incrementing count..\n", num)
+      numberCounts[num]++
+      // v, _ := numberCounts[num]
+      // fmt.Printf("Cache: numberCounts[%d] -> %d\n", num, v)
+
+        if numberCounts[num] > 3 {
+            // fmt.Printf("%d is found > 3 times - uh oh...", num)
+            // v, _ := numberCounts[num]
+            return fmt.Errorf("%d found >3 times.", num)
+        }
+    } else {        
+      numberCounts[num] = 1
+      // v, _ := numberCounts[num]
+      // fmt.Printf("%d is not in cache yet, setting.. numberCounts[%d] -> %d\n", num, v)
+    }
+  }
+  return nil
+}
+
 
 // return an error if any number is found to have been repeated more than once in the input collection 
 func detectRepeats(numbers []int) error {
