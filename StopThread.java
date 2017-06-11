b2518fd2bea8ef0c7ab21156c50ef4085d16fb18
Nirmal <astdb@users.noreply.github.com>
2017-06-12 08:17:31 +1000

    Initial commit

---
+++ StopThread.java
@@ -0,0 +1,31 @@
+
+// Effective Java, CH10/I66
+
+import java.util.concurrent.TimeUnit;
+
+public class StopThread {
+    private static boolean stopRequested;
+
+    private static synchronized void requestStop() {
+        stopRequested = true;
+    }
+
+    private static synchronized boolean stopRequested() {
+        return stopRequested;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread backgroundThread = new Thread ( new Runnable() {
+            public void run() {
+                int i = 0;
+                while(!stopRequested()) {
+                    i++;
+                }
+            }
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(2);
+        requestStop();
+    }
+}
