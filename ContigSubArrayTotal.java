bbcc71b31c8696dbb9d4b89294bda1dbfa4fac73
Nirmal <astdb@users.noreply.github.com>
2018-01-07 10:14:58 +1100

    Add more efficient implementation

--- ContigSubArrayTotal.java
+++ ContigSubArrayTotal.java
@@ -13,6 +13,28 @@ public class ContigSubArrayTotal {
         System.out.println(Arrays.toString(maxSubArray(input)));
     }
 
+    // implementing Kadane's algorithm, O(n) (https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/)
+    static int maxSubArrayEfficient(int[] nums) {
+        int size = nums.length;
+        int max_so_far = Integer.MIN_VALUE;
+        int max_ending_here = 0;
+ 
+        for (int i = 0; i < size; i++) {
+            max_ending_here = max_ending_here + nums[i];
+
+            if (max_so_far < max_ending_here) {
+                max_so_far = max_ending_here;
+            }
+
+            if (max_ending_here < 0) {
+                max_ending_here = 0;
+            }
+        }
+
+        return max_so_far;
+    }
+
+    // bruteforce O(n^2)
     public static int[] maxSubArray(int[] nums) {
         // array to store maximum subarray total and subarray boundaries e.g. max = {total, substart, subend+1}
         int[] maxSubTot = {0,0,0};
