c6baacfdad37fc539d3ac48d4fdcecd065badddf
Nirmal <astdb@users.noreply.github.com>
2018-01-13 19:03:52 +1100

    Update  MinMoves.go

--- MinMoves.go
+++ MinMoves.go
@@ -1,6 +1,6 @@
 
 /*
-    Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+    Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing (n-1) elements by 1.
 */
 
 package main
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	tests := [][]int{{1,2,3,}, {4,5,6,}, {7,8,9,}}
+	tests := [][]int{{}, {1}, {1,1}, {1,2,3,}, {4,5,6,}, {7,8,9,}, {1,2147483648}}
 
 	for _, testcase := range tests {
 		fmt.Printf("%v\t%d\n", testcase, minMoves(testcase))
@@ -19,37 +19,42 @@ func main() {
 
 func minMoves(nums []int) int {
 	// while not all array elements are equal
+	// fmt.Printf("\n-------------------\nStarting array: %v\n", nums)
 	moves := 0
     for !equalElements(nums) {
 		moves = moves + 1
-		highest := getHighest(nums)
-		fmt.Printf("\t%v\n", nums)
+		highestIndex := getHighestIndex(nums)
 
 		for i := 0; i < len(nums); i++ {
-			if nums[i] != highest {
+			if i != highestIndex {
 				nums[i] = nums[i] + 1
 			}
 		}
+
+		// fmt.Printf("\tMove %d, Array: %v\n", moves, nums)
 	}
 
 	return moves
 }
 
-func getHighest(n []int) int {
-	highest := n[0]
+func getHighestIndex(n []int) int {
+	if len(n) <= 0 {
+		return 0
+	}
 
-	for _, v := range n {
-		if v > highest {
-			highest = v
+	highestIndex := 0
+	for k,v := range n {
+		if v > n[highestIndex] {
+			highestIndex = k
 		}
 	}
 
-	return highest
+	return highestIndex
 }
 
 func equalElements(n []int) bool {
 	if len(n) <= 0 {
-		return false
+		return true
 	}
 
 	k := n[0]
