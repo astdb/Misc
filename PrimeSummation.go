655cffa89936322dc4d6430ae562731849b9029b
Nirmal <astdb@users.noreply.github.com>
2018-01-27 20:03:46 +1100

    Update PrimeSummation
    Move prime check to private function

--- PrimeSummation.go
+++ PrimeSummation.go
@@ -14,24 +14,33 @@ func main() {
 	tot := 0 // prime total
 
 	// start looking for primes from 2, until we've found a prime >= 2 000 000
-	for j := 2; j < 2000000; j++ {
-		if j == 2 || j == 3 {
-			// 2 is prime by definition - only 'even' prime
+	for j := 2; j < 10; j++ {
+		if isPrime(j) {
 			tot += j
-		} else {
-			// test j for divisability upto sqrt(j)
-			for k := 2; k <= int(math.Sqrt(float64(j))); k++ {
-				if j%k == 0 {
-					// j isn't prime - goto next j
-					break
-				}
-
-				if k == int(math.Sqrt(float64(j))) {
-					tot += j
-				}
-			}
 		}
 	}
 
 	fmt.Println(tot)
+}
+
+func isPrime(j int) bool {
+	if j == 2 || j == 3 {
+		// 2 is prime by definition - only 'even' prime
+		return true
+	} else {
+		// test j for divisability upto sqrt(j)
+		intSqrtJ := int(math.Sqrt(float64(j)))
+		for k := 2; k <= intSqrtJ; k++ {
+			if j%k == 0 {
+				// j isn't prime - goto next j
+				return false
+			}
+
+			if k == intSqrtJ {
+				return true
+			}
+		}
+	}
+
+	return false
 }
