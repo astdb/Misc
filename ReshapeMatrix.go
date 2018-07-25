448bc08871951bdda81743a11eeb8bd66b86753c
Nirmal <astdb@users.noreply.github.com>
2018-07-25 22:54:34 +1000

    Initial commit

---
+++ ReshapeMatrix.go
@@ -0,0 +1,84 @@
+/*
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+Example 1:
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output: 
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+Example 2:
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output: 
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// matrix1 := [][]int{{1,2,3},{4,5,6},{7,8,9}}
+	matrix2 := [][]int{{1,2},{3,4}}
+	fmt.Println(matrixReshape(matrix2, 1, 4))
+}
+
+func matrixReshape(nums [][]int, r int, c int) [][]int {
+	// expand original matrix
+	expandedMatrix := []int{}
+
+	for _, row := range nums {
+		for _, element := range row {
+			expandedMatrix = append(expandedMatrix, element)
+		}
+	}
+
+	// fmt.Println(expandedMatrix)
+	result := [][]int{}
+	row := 0
+	col := 0
+	i := 0
+
+	for row < r {
+		result = append(result, []int{})
+		for col < c {
+			result[row] = append(result[row], expandedMatrix[i])
+			col++
+			i++
+		}
+		row++
+		col = 0
+	}
+
+	// for i := 0; i < len(expandedMatrix); i++ {
+	// 	for row < r {
+	// 		result = append(result, []int{})
+	// 		for col < c {
+	// 			result[row] = append(result[row], i)
+	// 		}
+	// 		row++
+	// 		col = 0
+	// 	}
+	// }	
+
+	return result
+}
