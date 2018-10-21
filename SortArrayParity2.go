7ebf85dc9e08898fa6a2d096a122d8c79c0845da
Nirmal <astdb@users.noreply.github.com>
2018-10-21 18:07:01 +1100

    Initial commit

---
+++ SortArrayParity2.go
@@ -0,0 +1,57 @@
+/*
+Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
+Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+You may return any answer array that satisfies this condition.
+
+Example 1:
+Input: [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+Note:
+2 <= A.length <= 20000
+A.length % 2 == 0
+0 <= A[i] <= 1000
+
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := []int{4, 2, 5, 7}
+	fmt.Println(input, "=>", sortArrayByParityII(input))
+}
+
+func sortArrayByParityII(list []int) []int {
+	even := []int{}
+	odd := []int{}
+
+	for _, v := range list {
+		if v%2 == 0 {
+			even = append(even, v)
+		} else {
+			odd = append(odd, v)
+		}
+	}
+
+	result := []int{}
+	evenIndex := 0
+	oddIndex := 0
+	for i := 0; i < len(list); i++ {
+		// fmt.Println("i = ", i)
+		if i%2 == 0 {
+			// fmt.Println("\tEven i, adding even element")
+			result = append(result, even[evenIndex])
+			evenIndex++
+		} else {
+			// fmt.Println("\tEven i, adding odd element")
+			result = append(result, odd[oddIndex])
+			oddIndex++
+		}
+	}
+
+	return result
+}
