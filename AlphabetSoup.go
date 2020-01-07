302c2b90f89d20ee2c90b8af8c3a081238fe8d6a
Nirmal <astdb@users.noreply.github.com>
2020-01-07 20:30:42 +1100

    First working version

--- AlphabetSoup.go
+++ AlphabetSoup.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -34,20 +33,101 @@ func main() {
 	for inputFileScanner.Scan() {
 		if lineNo == 0 {
 			// read testcase count
-			testCases, err := strconv.Atoi(inputFileScanner.Text())
+			testCases, err = strconv.Atoi(inputFileScanner.Text())
 			if err != nil {
 				log.Fatal(err)
-				
+
 			}
-			
-			str.WriteString(fmt.Sprintf("%d\n", testCases))
+
+			// str.WriteString(fmt.Sprintf("%d\n", testCases))
 			lineNo++
 			continue
 		} else {
 			// read test case
 			testCase := strings.TrimSpace(inputFileScanner.Text())
 
-			str.WriteString(fmt.Sprintf("%s\n", testCase))
+			// go through testcase char by char, and count the number of times each letter was seen
+			// need two C's for every single HACKERCUP, so count C's in doubles.
+			letterCounts := []int{0, 0, 0, 0, 0, 0, 0, 0}
+			subC := 0
+
+			for _, ch := range testCase {
+				if string(ch) == "H" {
+					letterCounts[0]++
+				}
+
+				if string(ch) == "A" {
+					letterCounts[1]++
+				}
+
+				if string(ch) == "C" {
+					subC++
+
+					if subC == 2 {
+						letterCounts[2]++
+						subC = 0
+					}
+					
+				}
+
+				if string(ch) == "K" {
+					letterCounts[3]++
+				}
+
+				if string(ch) == "E" {
+					letterCounts[4]++
+				}
+
+				if string(ch) == "R" {
+					letterCounts[5]++
+				}
+
+				if string(ch) == "U" {
+					letterCounts[6]++
+				}
+
+				if string(ch) == "P" {
+					letterCounts[7]++
+				}
+			}
+
+			// formations := 0
+			// if allLettersFound(letterCounts) {
+			// 	cCount := letterCounts[2]
+			// 	min := 0
+			// 	for i, j := range letterCounts {
+			// 		if i == 0 {
+			// 			min = j
+			// 		} else {
+			// 			if j < min {
+			// 				min = j
+			// 			}
+			// 		}
+			// 	}
+
+			// 	if cCount >= (min * 2) {
+			// 		formations = cCount
+			// 	} else if min >= (cCount / 2) {
+			// 		formations = cCount / 2
+			// 	} else {
+			// 		log.Printf("Weird situation: min == %d, cCount == %d, formations == %d\n", min, cCount, formations)
+			// 	}
+			// }
+
+			// find the least occuring required letter - that would be the number of full HACKERCUPs that can be formed with the letters alread in the soup
+			possibleWordFormations := 0
+
+			for k, v := range letterCounts {
+				if k == 0 {
+					possibleWordFormations = v
+				} else {
+					if v < possibleWordFormations {
+						possibleWordFormations = v
+					}
+				}
+			}
+
+			str.WriteString(fmt.Sprintf("Case #%d: %d\n", lineNo, possibleWordFormations))
 		}
 
 		if lineNo > testCases {
@@ -71,4 +151,14 @@ func main() {
 	}
 
 	outputFile.Sync()
+}
+
+func allLettersFound(letters []int) bool {
+	for _, i := range letters {
+		if i <= 0 {
+			return false
+		}
+	}
+
+	return true
 }
