88c9adb10734c275d1a1d9f275a4e7f75dbf2f5d
Nirmal <astdb@users.noreply.github.com>
2020-02-19 14:52:09 +1100

    Add multiple folder input/log writing

--- FileCopy.go
+++ FileCopy.go
@@ -1,5 +1,5 @@
-// fc copies a specified directory with its contents (file only) from a specified source path to a specified destination path. 
-// file copy functions from https://stackoverflow.com/questions/21060945/simple-way-to-copy-a-file-in-golang/21067803#21067803
+// fc copies a specified set of directories with its contents (file only) from a specified source path to a specified destination path.
+// filecopy functions from https://stackoverflow.com/questions/21060945/simple-way-to-copy-a-file-in-golang/21067803#21067803
 
 package main
 
@@ -15,15 +15,28 @@ import (
 
 func main() {
 	if len(os.Args) < 4 {
-		log.Fatal("Usage: $>fc.exe \"sourcepath\" \"destpath\" \"foldername\"")
+		log.Fatal("Usage: $>fc.exe \"sourcepath\" \"destpath\" \"foldername1, foldername2, foldername3\"")
 	}
 
+	// write log (https://stackoverflow.com/a/51628140)
+	logFilename := fmt.Sprintf("fc_log_%s.log", time.Now().Format("2006-01-02_15-04-05"))
+	logFile, err := os.OpenFile(logFilename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error creting logfile: %v", err)
+	}
+	defer logFile.Close()
+
+	wrt := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(wrt)
+	// log.Println(" Orders API Called")
+
+	// read command line args
 	rootDir := strings.TrimSpace(os.Args[1])
 	destRoot := strings.TrimSpace(os.Args[2])
-	targetDir := strings.TrimSpace(os.Args[3])
+	targetDirs := strings.Split(strings.TrimSpace(os.Args[3]), ",")
 
-	log.Printf("fc(): Copying contents of <%s> directory from <%s> to <%s>...", targetDir, rootDir, destRoot)
-	time.Sleep(time.Duration(8) * time.Second)
+	log.Printf("fc(): Copying contents of <%v> directory/directories from <%s> to <%s>...", targetDirs, rootDir, destRoot)
+	time.Sleep(time.Duration(4) * time.Second)
 	log.Println("fc(): starting copy..")
 
 	files, err := ioutil.ReadDir(rootDir)
@@ -31,13 +44,13 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// for each subfolder in the root dir
 	for _, f := range files {
-		// fmt.Println(f.Name())
+		fmt.Println(f.Name())
 
-		// each of these is a folder name as well
-		// create dir
-		if f.Name() == targetDir {
-			log.Printf("fc(): found %s in %s\n", targetDir, rootDir)
+		// check if this subfolder is in the list of foldernames in the input: if so, copy to destination
+		if toBeCopied(f.Name(), targetDirs) {
+			log.Printf("------------------------------------------------------ \nfc(): found <%s> folder in root directory <%s>, copying..\n", f.Name(), rootDir)
 			path := fmt.Sprintf("%s\\%s", destRoot, f.Name())
 			log.Printf("\n\nfc(): creating destination folder %s..\n", path)
 			if _, err := os.Stat(path); os.IsNotExist(err) {
@@ -50,20 +63,36 @@ func main() {
 				log.Fatal(err2)
 			}
 
+			fileNum := len(files2)
+			fileCount := 1
+
 			for _, f2 := range files2 {
 				sourceFile := fmt.Sprintf("%s\\%s", fPath, f2.Name())
 				destFile := fmt.Sprintf("%s\\%s", path, f2.Name())
 
-				log.Printf("\n\nfc(): copying %s to %s\n", sourceFile, destFile)
+				log.Printf("\n\tfc(): copying <%s> to <%s> (file %d of %d)\n", sourceFile, destFile, fileCount, fileNum)
 				err := CopyFile(sourceFile, destFile)
 				if err != nil {
-					fmt.Printf("fc(): copy failed %q\n", err)
+					fmt.Printf("\n\tfc(): ***************** copy failed %q *********************** \n", err)
 				} else {
-					fmt.Printf("fc(): copy succeeded\n")
+					fmt.Printf("\tfc(): copy succeeded.\n")
+					fileCount++
 				}
 			}
 		}
 	}
+}
+
+func toBeCopied(folderName string, folders []string) bool {
+	// log.Printf("toBeCopied(): checking if %s is in %v..", folderName, folders)
+	for _, folder := range folders {
+		if strings.TrimSpace(folderName) == strings.TrimSpace(folder) {
+			// log.Printf("toBeCopied(): %s is in the list of directories to be copied.", folderName)
+			return true
+		}
+	}
+
+	return false
 }
 
 // CopyFile copies a file from src to dst. If src and dst files exist, and are
