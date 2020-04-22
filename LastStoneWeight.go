fdd76d32055302b672afd93e1d4d012698e2b9e3
Nirmal <astdb@users.noreply.github.com>
2020-04-22 20:44:00 +1000

    Fix bugs - working version

--- LastStoneWeight.go
+++ LastStoneWeight.go
@@ -31,17 +31,45 @@ package main
 import (
 	"errors"
 	"log"
+	"sort"
 )
 
 func main() {
-	tests := [][]int{{2, 7, 4, 1, 8, 1}, {1,3}}
+	tests := [][]int{{2, 7, 4, 1, 8, 1}, {1,3}, {9,3,2,10}}
+	// tests := [][]int{{9,3,2,10}}
 
 	for _, test := range tests {
-		log.Printf("lastStoneWeight(%v) == %d\n", test, lastStoneWeight(test))
+		log.Printf("lastStoneWeight2(%v) == %d\n", test, lastStoneWeight2(test))
 	}
 }
 
 func lastStoneWeight(stones []int) int {
+	// sort stones by weight
+	sort.Ints(stones)
+
+	for len(stones) > 1 {
+		// smash largest
+		smash := stones[len(stones)-1] - stones[len(stones)-2]
+		
+		// copy over, replacing two largest with smash result
+		newStones := []int{}
+		for i := 0; i < (len(stones)-2); i++ {
+			newStones = append(newStones, stones[i])
+		}
+
+		newStones = append(newStones, smash)
+		stones = newStones
+		sort.Ints(stones)
+	}
+
+	if len(stones) <= 0 {
+		log.Fatal("len(stones) <= 0")
+	}
+
+	return stones[0]
+}
+
+func lastStoneWeight2(stones []int) int {
 	for len(stones) > 1 {
 		log.Printf("==============================")
 		log.Printf("Stones: %v\n", stones)
@@ -54,13 +82,16 @@ func lastStoneWeight(stones []int) int {
 		log.Printf("Largest/index: %d/%d\n", largestVal, largestIndex)
 
 		// find second largest value
-		secLargestIndex, secLargestVal, err := secLargest(stones, largestVal)
+		secLargestIndex, secLargestVal, err := secLargest(stones, largestIndex)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		log.Printf("Sec. largest/index: %d/%d\n", secLargestVal, secLargestIndex)
+
 		// smash result
 		smashRes := largestVal - secLargestVal
+		log.Printf("SmashRes: %d\n", smashRes)
 
 		// create new stones array and copy all elements in except largest and second largest: copy smash result instead
 		newStones := []int{}
@@ -72,7 +103,10 @@ func lastStoneWeight(stones []int) int {
 		}
 
 		newStones = append(newStones, smashRes)
+		log.Printf("New Stones: %v\n", newStones)
+
 		stones = newStones
+		log.Printf("STONES: %v\n\n", stones)
 
 	}
 
@@ -83,25 +117,34 @@ func lastStoneWeight(stones []int) int {
 	return stones[0]
 }
 
-// given an array of ints, and it's largest value, return the index and value of second largest element
-func secLargest(x []int, largestVal int) (int, int, error) {
+// given an array of ints, and it's largest value's index, return the index and value of second largest element
+func secLargest(x []int, largestValIndex int) (int, int, error) {
 	var secLargestVal int
 	var secLargestIndex int
+
+	log.Printf("\tsecLargest(): largestValIndex: %d\n", largestValIndex)
 
 	if len(x) <= 0 {
 		return secLargestIndex, secLargestVal, errors.New("secLargest(): empty input array.")
 	}
 
+	first := true
 	for k, v := range x {
-		first := true
-		if v <= largestVal {
+		// if v <= largestVal {
+		if k != largestValIndex {
 			// second largest value candidate
+			log.Printf("\tsecLargest(): considering x[%d] == %d as second largest candidate..\n", k, v)
+			
 			if first {
+				log.Printf("\tsecLargest(): initializing x[%d] == %d to sec largest val\n", k, v)
 				secLargestVal = v
 				secLargestIndex = k
 				first = false
 			} else {
 				if v > secLargestVal {
+
+					log.Printf("\tsecLargest(): competing second largest value found at x[%d] == %d, resetting prev val..\n")
+
 					secLargestVal = v
 					secLargestIndex = k
 				}
