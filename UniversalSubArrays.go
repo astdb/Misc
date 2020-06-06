1bd0e83c0fd717dab6c458e17c98503303052151
Nirmal <astdb@users.noreply.github.com>
2020-06-06 21:34:44 +1000

    First working iteration

--- UniversalSubArrays.go
+++ UniversalSubArrays.go
@@ -8,17 +8,17 @@ A universal sub-array is one that consists of a contiguous set of 2's and then a
 package main
 
 import (
-	// "log"
-	"fmt"
+	"log"
+	// "fmt"
 )
 
 func main() {
-	// tests := [][]int{{1,2,3,4}, {4,4,2,2,4,2}}
+	tests := [][]int32{{1,2,3,4}, {4,4,2,2,4,2}}
 
-	// for _, test := range tests {
-	// 	// log.Printf("countingUniversalSubarrays(%v) == %v\n", test, countingUniversalSubarrays(test))
-	// 	log.Printf();
-	// }
+	for _, test := range tests {
+		log.Printf("countingUniversalSubarrays(%v) == %v\n", test, countingUniversalSubarrays(test))
+		// log.Printf();
+	}
 
 	// res := [][]int32{}
 	// printSubArrays([]int32{1,2,3,4}, 0, 0, res)
@@ -27,9 +27,15 @@ func main() {
 
 	// x := []int{}
 
-	res := NewRes()
-	printSubArrays([]int32{1,2,3,4}, 0, 0, res)
-	fmt.Println(res.Result)
+	// res := NewRes()
+	// getSubArrays([]int32{1,2,3,4}, 0, 0, res)
+	// fmt.Println(res.Result)
+
+	// tests := [][]int32{{4,2}, {2,4}, {4,4,2,2}, {2,2,4,4}, {4,4,4,2,2,2}, {2}, {4}, {2,2,4}, {4,2,4}, {4,5,2,2}}
+
+	// for _, test := range tests {
+	// 	fmt.Printf("isUniversal(%v) == %v\n", test, isUniversal(test))
+	// }
 }
 
 func testFunc(x []int) {
@@ -39,17 +45,43 @@ func testFunc(x []int) {
 	x = append(x, 4)
 }
 
-// func countingUniversalSubarrays(arr []int32) int32 {
-//     // Write your code here
-// 	start := 0
-// 	end := 0
+func countingUniversalSubarrays(arr []int32) int32 {
+    // Write your code here
+	res := NewRes()
+	// getSubArrays([]int32{1,2,3,4}, 0, 0, res)
+	getSubArrays(arr, 0, 0, res)
 
-// 	if end >= len(arr) {
-// 		return 0
-// 	} else if start > end {
+	var universalCount int32
+	universalCount = 0
+	for _, arr := range res.Result {
+		if isUniversal(arr) {
+			universalCount++
+		}
+	}
 
-// 	}
-// }
+	return universalCount
+}
+
+func getSubArrays(arr []int32, start, end int, res *Res) {
+	if end >= len(arr) {
+		return
+
+	} else if start > end {
+		getSubArrays(arr, 0, end + 1, res)
+
+	} else {
+		thisSubArr := []int32{}
+
+		for i := start; i < end; i++ {
+			thisSubArr = append(thisSubArr, arr[i])
+		}
+
+		thisSubArr = append(thisSubArr, arr[end])
+		res.Result = append(res.Result, thisSubArr)
+
+		getSubArrays(arr, start + 1, end, res)
+	}
+}
 
 type Res struct {
 	Result [][]int32
@@ -65,58 +97,56 @@ func isUniversal(arr []int32) bool {
 		return false
 	}
 
-	twoSect := false
-	sectorChanged := false
+	if len(arr) % 2 != 0 {
+		return false
+	}
 
 	twoCount := 0
 	fourCount := 0
+	switched := false
+	inTwos := false
+	inFours := false
 
 	for i := 0; i < len(arr); i++ {
+		if !(arr[i] == 2 || arr[i] == 4) {
+			return false
+		}
+
 		if i == 0 {
 			if arr[i] == 2 {
-				twoSect = true
+				inTwos = true
 				twoCount++
 			} else if arr[i] == 4 {
-				twoSect = true
+				inFours = true
 				fourCount++
+			} else {
+				log.Printf("\tisUniversal(): i == 0, unexpected arr val, returning false")
+				return false
+			}
+		} else {
+			if inTwos && arr[i] == 2 {
+				twoCount++
+			} else if inFours && arr[i] == 4 {
+				fourCount++
+			} else if inTwos && arr[i] == 4 && !switched {
+				inTwos = false
+				inFours = true
+				switched = true
+				fourCount++
+			} else if inFours && arr[i] == 2 && !switched {
+				inFours = false
+				inTwos = true
+				switched = true
+				twoCount++
 			} else {
 				return false
 			}
 		}
-
-		if arr[i] == 2 && twoSect {
-			twoCount++
-		}
-
-		if arr[i] == 4 && fourSect {
-			fourCount++
-		}
-
-
 	}
-}
 
-func getSubArrays(arr []int32, start, end int, res *Res) {
-	// start := 0
-	// end := 0
-
-	if end >= len(arr) {
-		return
-	} else if start > end {
-		printSubArrays(arr, 0, end+1, res)
-	} else {
-		thisArr := []int32{}
-		// fmt.Printf("[")
-
-		for i := start; i < end; i++ {
-			// fmt.Printf("%d, ", arr[i])
-			thisArr = append(thisArr, arr[i])
-		}
-
-		// fmt.Printf("%d]", arr[end])
-		thisArr = append(thisArr, arr[end])
-		res.Result = append(res.Result, thisArr)
-
-		printSubArrays(arr, start+1, end, res)
+	if twoCount == fourCount {
+		return true
 	}
+
+	return false
 }
