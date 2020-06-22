8a8eeb8f1fd5a9b2902158a1c6753a82632256fc
Nirmal <astdb@users.noreply.github.com>
2020-06-22 21:14:34 +1000

    Updates

--- ArrThreePartitionsEqSum.go
+++ ArrThreePartitionsEqSum.go
@@ -35,13 +35,49 @@ import (
 
 func main() {
   tests := [][]int{{3,3,2,6,2}, {0}, {0,0}, {1,2,3,4,5,6}, {1,2,3,9,4,5,6}}
+  tests2 := [][]int{{0,2,1,-6,6,7,9,-1,2,0,1}, {0,2,1,-6,6,7,9,-1,2,0,1}, {3,3,6,5,-2,2,5,1,-9,4}}
 
   for _, test := range tests {
       log.Printf("TwoPartsEqSum(%v) == %v\n", test, TwoPartsEqSum(test))
   }
+
+  for _, test := range tests2 {
+      log.Printf("ThreePartsEqSum(%v) == %v\n", test, canThreePartsEqualSum(test))
+  }
 }
 
 func canThreePartsEqualSum(A []int) bool {
+  if len(A) < 3 {
+    // cannot split A into three non-empty parts
+    return false
+  }
+
+  aTotal := sumArr(A)
+  if aTotal % 3 != 0 {
+    // total of A's elements cannot be split into three
+    return false
+  }
+
+  third := aTotal/3
+  partSum := 0  // partial sum of A's elements up to i'th
+  x1 := false   // flag indicating if there's a point in A where the sum of preceding elements == third
+  x2 := false   // flag indicating if there's a point in A where the sum of preceding elements == (2*third)
+  for i := 0; i < len(A); i++ {
+    partSum += A[i]
+
+    if partSum == third {
+      x1 = true
+    }
+
+    if partSum == (2*third) {
+      x2 = true
+    }
+  }
+
+  if x1 && x2 {
+    return true
+  }
+
   return false
 }
 
