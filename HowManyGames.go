938c9dd852107c00289395ac2d191aa3df590a26
Nirmal <astdb@users.noreply.github.com>
2020-07-17 00:05:49 +1000

    Working version

--- HowManyGames.go
+++ HowManyGames.go
@@ -18,13 +18,13 @@ func howManyGames(p int32, d int32, m int32, s int32) int32 {
 	// Return the number of games you can buy
 	var gameCount int32
 	gameCount = 0
-	
-  var prevPrice int32 // price of last game bought
-  // prevPrice = p
+
+	var prevPrice int32 // price of last game bought
+	// prevPrice = p
 
 	// buy first game if wallet > starter price. else return 0 (gameCount initial value)
 	if s >= p {
-    log.Printf("Buying first game for $%d\n", p)
+		// log.Printf("Buying first game for $%d\n", p)
 		gameCount++
 		s = s - p
 		prevPrice = p
@@ -32,15 +32,21 @@ func howManyGames(p int32, d int32, m int32, s int32) int32 {
 		return gameCount
 	}
 
-  
 	for s > 0 {
 		thisGamePrice := prevPrice - d
 		if thisGamePrice <= m {
 			thisGamePrice = m
 		}
 
+		// log.Printf("Buying next game for $%d (prev game bought for $%d)\n", thisGamePrice, prevPrice)
+
 		s = s - thisGamePrice
 		prevPrice = thisGamePrice
+
+		if s >= 0 {
+			gameCount++
+		}
+		// gameCount++
 	}
 
 	return gameCount
