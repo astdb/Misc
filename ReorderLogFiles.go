1e75b5d12baa89f9d5d1c83e2b48ff2e546e1a0c
Nirmal <astdb@users.noreply.github.com>
2020-09-15 07:10:47 +1000

    Initial commit

---
+++ ReorderLogFiles.go
@@ -0,0 +1,45 @@
+/*
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+ 
+
+Example 1:
+
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+ 
+
+Constraints:
+
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] is guaranteed to have an identifier, and a word after the identifier.
+*/
+
+package main
+
+import (
+  "log"
+)
+
+func main() {
+  
+}
+
+func reorderLogFiles(logs []string) []string {
+    // each log is a string of tokens
+    // first token is an alphanumeric ID.
+    // content after token is either all alphabetic (letter-log) or all numeric (digit-log).
+    // order letter logs first, lexicographically ignoring ID (use ID only for tiiebreaks). 
+    // digit logs must come after all letter logs, in original order.
+}
