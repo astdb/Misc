b970e31684a1de94338009c40ca7e71b6f2b76ab
Nirmal <astdb@users.noreply.github.com>
2020-09-20 19:47:08 +1000

    Update FindKthPositive.go

--- FindKthPositive.go
+++ FindKthPositive.go
@@ -28,13 +28,63 @@ arr[i] < arr[j] for 1 <= i < j <= arr.length
 
 package main
 
+import (
+  "log"
+)
+
 func main() {
- tests := [][]int{{2,3,4,7,11}, {1,2,3,4}} 
+ tests := [][][]int{{{2,3,4,7,11},{5}}, {{1,2,3,4},{2}}} 
   for _, test := range tests {
-    log.Printf("findKthPositive(%v) = %d\n", findKthPositive(test))
+    log.Printf("findKthPositive(%v, %d) = %d\n", findKthPositive(test[0], test[1][0]), test[0], test[1][0])
   }
 }
 
 func findKthPositive(arr []int, k int) int {
-    
+  intCount := 1
+  missingCount := 0
+  arrIndex := 0
+
+  for missingCount < k {
+    if arrIndex < len(arr) {
+      if arr[arrIndex] == intCount {
+        // not a missing int - increment both int and array index
+        intCount++
+        arrIndex++
+  
+      } else {
+        // current index value is bigger than current int - increase index until arr[index] == int (and count missing ints)
+        if arr[arrIndex] > intCount {
+            for intCount < arr[arrIndex] {
+              missingCount++
+              intCount++
+  
+              if missingCount == k {
+                return intCount
+              }
+            }
+  
+            // at this point, arr[index] == int
+            intCount++
+            arrIndex++
+        } else {
+          // arr[index] < int : cannot happen?
+        }
+  
+  
+      }
+    } else {
+      for missingCount < k {
+        missingCount++
+        intCount++
+
+        if missingCount == k {
+           return intCount
+        }
+      }
+    }   
+
+    // intCount++
+  }
+
+  return intCount
 }
