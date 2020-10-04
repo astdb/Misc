30a05d22a63585d37959aea16f61f38f025d11eb
Nirmal <astdb@users.noreply.github.com>
2020-10-04 18:22:03 +1100

    Implement linear space version

--- ReverseVowels.go
+++ ReverseVowels.go
@@ -23,8 +23,26 @@ import (
 func main() {
   tests := []string{"hello", "leetcode"}
   for _, test := range tests {
-    log.Printf("reverseVowels(%s) = %s\n", test, reverseVowels(test))
+    // log.Printf("reverseVowels(%s) = %s\n", test, reverseVowels(test))
+    log.Printf("reverseVowels(%s) = %s\n", test, reverseVowels2(test))
   }
+}
+
+func reverseVowels2(s string) string {
+  sRunes := []rune(s)
+  vowelIndexList := []int{}
+
+  for i := 0; i < len(sRunes); i++ {
+    if isVowel(sRunes[i]) {
+      vowelIndexList = append(vowelIndexList, i)
+    }
+  }
+
+  for i := 0; i < len(vowelIndexList)/2; i++ {
+    sRunes[vowelIndexList[i]], sRunes[vowelIndexList[len(vowelIndexList)- i - 1]] = sRunes[vowelIndexList[len(vowelIndexList)- i - 1]], sRunes[vowelIndexList[i]]
+  }
+
+  return string(sRunes)
 }
 
 func reverseVowels(s string) string {
