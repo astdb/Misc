7f6df3efb328af58d3f7a7ef37132a23e45baa8d
Nirmal <astdb@users.noreply.github.com>
2020-10-07 22:33:45 +1100

    Updates

--- ShuffleArr.go
+++ ShuffleArr.go
@@ -3,12 +3,12 @@ Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,.
 
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 
- 
+
 
 Example 1:
 
 Input: nums = [2,5,1,3,4,7], n = 3
-Output: [2,3,5,4,1,7] 
+Output: [2,3,5,4,1,7]
 Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
 Example 2:
 
@@ -18,7 +18,7 @@ Example 3:
 
 Input: nums = [1,1,2,2], n = 2
 Output: [1,2,1,2]
- 
+
 
 Constraints:
 
@@ -31,13 +31,38 @@ nums.length == 2n
 package main
 
 import (
-  "log"
+	"log"
 )
 
 func main() {
+	tests := [][][]int{{{2, 5, 1, 3, 4, 7}, {3}}, {{1, 2, 3, 4, 4, 3, 2, 1}, {4}}, {{1, 1, 2, 2}, {2}}}
 
+	for _, test := range tests {
+		log.Printf("shuffle(%v, %d) = %v\n", test[0], test[1][0], shuffle(test[0], test[1][0]))
+	}
 }
 
 func shuffle(nums []int, n int) []int {
-     
+	i := 0
+	j := len(nums) / 2
+	first := true
+	nCount := 0
+
+	res := []int{}
+	for nCount < n && i < len(nums)/2 && j < len(nums) {
+		if first {
+			res = append(res, nums[i])
+
+			i++
+			first = false
+		} else {
+			res = append(res, nums[j])
+
+			j++
+			first = true
+			nCount++
+		}
+	}
+
+	return res
 }
