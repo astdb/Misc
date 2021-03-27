4b0ff037f3d74f346c643fc0544fd4d5061aa461
Nirmal <astdb@users.noreply.github.com>
2021-03-27 17:08:50 +1100

    Update RotateImage.go

--- RotateImage.go
+++ RotateImage.go
@@ -2,6 +2,7 @@
 Given an image represented by a NxN matrix, rotate it by 90degrees.
 
 Can this be done in place?
+
 */
 
 package main
@@ -11,23 +12,13 @@ import (
 )
 
 func main() {
-	// tests := [][][]int{{{1}}, {{1, 2}, {3, 4}}, {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}}}
-  tests := [][]int{{}, {1}, {1,2}, {1,2,3}}
+	tests := [][][]int{{{1}}, {{1, 2}, {3, 4}}, {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}}}
 
 	for _, test := range tests {
 		// log.Printf("cols(%v) = %v\n", test, cols(test))
-		// log.Printf("cols(%v) = %v\n", test, rotate(test))
+		log.Printf("cols(%v) = %v\n", test, rotate(test))
 
-    log.Printf("%v\n", revArr(test))
 	}
-}
-
-func revArr(arr []int) []int {
-  for i := 0; i < len(arr)/2;  i++ {
-    arr[i], arr[len(arr)-1] = arr[len(arr)-1], arr[i]
-  }
-
-  return arr
 }
 
 func rotate(img [][]int) [][]int {
@@ -35,8 +26,8 @@ func rotate(img [][]int) [][]int {
 
 	for i := 0; i < len(img); i++ {
 		// reverse img[i]
-		for j := 0; j <= len(img[i])/2; j++ {
-			imgCols[i][j], imgCols[i][len(imgCols[i])-1] = imgCols[i][len(imgCols[i])-1], imgCols[i][j]
+		for j := 0; j < len(img[i])/2; j++ {
+			imgCols[i][j], imgCols[i][len(imgCols[i])-j-1] = imgCols[i][len(imgCols[i])-j-1], imgCols[i][j]
 		}
 	}
 
