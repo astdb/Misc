275678d5086dbcbe64db16b9383f4b60e054f65a
Nirmal <astdb@users.noreply.github.com>
2021-03-29 20:12:41 +1100

    Initial commit

---
+++ ZeroMatrix.go
@@ -0,0 +1,58 @@
+/*
+Given an NxN matrix, set each column and row to zero if any element within is zero.
+
+*/
+
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	tests := [][][]int{{{1, 2, 3, 4}, {5, 6, 0, 8}, {9, 0, 11, 12}, {13, 14, 15, 16}}}
+
+	for _, test := range tests {
+		log.Println("===================================================")
+		printMatrix(test)
+		log.Println("-------------")
+		// log.Printf("%v\n", zeroMatrix(test))
+		zeroedMat := zeroMatrix(test)
+		printMatrix(zeroedMat)
+	}
+}
+
+func zeroMatrix(mat [][]int) [][]int {
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[i]); j++ {
+			// if an element is zero, set the start of that row and top of that column to zero
+			if mat[i][j] == 0 {
+				mat[i][0] = 0
+				mat[0][j] = 0
+			}
+		}
+	}
+
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat); j++ {
+			if mat[i][0] == 0 || mat[0][j] == 0 {
+				mat[i][j] = 0
+			}
+		}
+	}
+
+	return mat
+}
+
+func printMatrix(mat [][]int) {
+	// log.Println(mat)
+
+	for i := 0; i < len(mat); i++ {
+		// for j := 0; j > len(mat[i]); j++ {
+		//   log.Printf("%d ", mat[i][j])
+		// }
+		// log.Printf("\n")
+
+		log.Println(mat[i])
+	}
+}
